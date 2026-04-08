fec075e742129574d9c4c7a69c438a3a57307161
yashh1204 <149853569+yashh1204@users.noreply.github.com>
2026-04-08 20:41:09 +0000

    made the play button

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// add javascript here
+let rawName = prompt("What is your name?");
+const promptGuess = document.getElementById("msg");let playerName = rawName.charAt(0).toUpperCase() + rawName.slice(1).toLowerCase();
+document.getElementById("playBtn").addEventListener("click", play);
+document.getElementById("guessBtn").addEventListener("click", makeGuess);
+document.getElementById("giveUpBtn").addEventListener("click", giveUp);
+function play() {
+promptGuess.textContent = "Hello " + playerName + "! I am thinking of a number between 1 and 100. Can you guess it?";
+document.getElementById("guessBtn").disabled = false;
+document.getElementById("giveUpBtn").disabled = false;
+document.getElementById("playBtn").disabled = true;
+window.secretNumber = Math.floor(Math.random() * 100) + 1;
+window.attempts = 0;
+}
